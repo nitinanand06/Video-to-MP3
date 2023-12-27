ffe0d9ceb2cdae6365ca1fcfe51a09aa42de533e
Nitin Anand <125756622+nitinanand06@users.noreply.github.com>
2023-12-27 11:41:24 -0500

    VMP3-3

--- videoToMP3.py
+++ videoToMP3.py
@@ -1,32 +1,58 @@
 #----------------------------------------------------------------------------------------------------------
 #import necessary packages
 #----------------------------------------------------------------------------------------------------------
-import yt_dlp as youtube_dl
+import os
+from pytube import YouTube
 
 #----------------------------------------------------------------------------------------------------------
 # Create a function to download songs
 #----------------------------------------------------------------------------------------------------------
 
-def download_songs(url):
-    # Create a YoutubeDL object with options
-    ydl_opts = {
-                'format': 'bestaudio/best',  # This selects the best audio-only format available
-                'outtmpl': 'C:/Songs/%(title)s.%(ext)s',  # Save each song with its title and .ext extension
-                }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(url)
+def clean_title(title):
+    cleaned_title = ''.join(e for e in title if e.isalnum() or e.isspace())
+    return cleaned_title.strip()
     
+def download_and_rename_audio(url, output_path):
+    try:
+        # Create a YouTube object
+        yt = YouTube(url)
 
+        # Get the audio stream with specified parameters
+        audio_stream = yt.streams.filter(only_audio=True, mime_type="audio/mp4", abr="128kbps").first()
+
+        # Download the audio
+        downloaded_file = audio_stream.download(output_path)
+
+        # Get the cleaned title
+        cleaned_title = clean_title(yt.title)
+
+        # Construct the new path with the cleaned title and original extension
+        new_file_path = os.path.join(output_path, f'{cleaned_title}.mp3')
+
+        # Rename the file with the cleaned title
+        os.rename(downloaded_file, new_file_path)
+
+        print(f"Audio downloaded, renamed, and information saved successfully: {cleaned_title}")
+
+    except Exception as e:
+        print(f"Error: {e}")
 #----------------------------------------------------------------------------------------------------------
 # Call the function
 #----------------------------------------------------------------------------------------------------------
 
 try:
-    url = "https://www.youtube.com/watch?v=87JIOAX3njM"
-    download_songs(url)
-    print("Song downloaded successfully! ")
+    file_path = 'Songs_List.txt'
+    output_path = 'C:/Songs/'
+    with open(os.path.join(output_path, file_path), 'r') as file:
+        urls = file.readlines()
+        for url in urls:
+            url = url.strip()  # Remove any leading/trailing whitespace or newline characters
+            if url:  # Check if the URL is not empty
+                download_and_rename_audio(url, output_path)
+            else:
+                print("File is empty!!")
 
 except Exception as e:
-    print(e)
+    print(f"Error reading URLs from file: {e}")
 
 
