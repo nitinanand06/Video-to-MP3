849a4c0b160802af7f4c00f24b41149d88592949
Nitin Anand <125756622+nitinanand06@users.noreply.github.com>
2023-12-29 13:45:17 -0500

    VMP3-4

--- videoToMP3.py
+++ videoToMP3.py
@@ -1,17 +1,11 @@
-#----------------------------------------------------------------------------------------------------------
-#import necessary packages
-#----------------------------------------------------------------------------------------------------------
 import os
 from pytube import YouTube
-
-#----------------------------------------------------------------------------------------------------------
-# Create a function to download songs
-#----------------------------------------------------------------------------------------------------------
+from pytube.exceptions import RegexMatchError, VideoUnavailable, PytubeError
 
 def clean_title(title):
     cleaned_title = ''.join(e for e in title if e.isalnum() or e.isspace())
     return cleaned_title.strip()
-    
+
 def download_and_rename_audio(url, output_path):
     try:
         # Create a YouTube object
@@ -32,27 +26,67 @@ def download_and_rename_audio(url, output_path):
         # Rename the file with the cleaned title
         os.rename(downloaded_file, new_file_path)
 
-        print(f"Audio downloaded, renamed, and information saved successfully: {cleaned_title}")
+        print(f"Audio downloaded, and renamed successfully: {cleaned_title}")
 
-    except Exception as e:
+    except RegexMatchError:
+        print("Error: Invalid URL format. Please enter a valid YouTube video URL.")
+    except VideoUnavailable:
+        print("Error: The video is unavailable or restricted. Please choose another video.")
+    except PytubeError as e:
         print(f"Error: {e}")
-#----------------------------------------------------------------------------------------------------------
-# Call the function
-#----------------------------------------------------------------------------------------------------------
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
-try:
-    file_path = 'Songs_List.txt'
-    output_path = 'C:/Songs/'
-    with open(os.path.join(output_path, file_path), 'r') as file:
-        urls = file.readlines()
-        for url in urls:
-            url = url.strip()  # Remove any leading/trailing whitespace or newline characters
-            if url:  # Check if the URL is not empty
-                download_and_rename_audio(url, output_path)
-            else:
-                print("File is empty!!")
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-except Exception as e:
-    print(f"Error reading URLs from file: {e}")
+def print_menu():
+    clear_screen()
+    print("+" + "-"*38 + "+")
+    print("|              CUI MENU               |")
+    print("+" + "-"*38 + "+")
+    print("| Options:                            |")
+    print("| 1. Fetch audio from a URL           |")
+    print("| 2. Read URLs from a file            |")
+    print("| 3. Exit                             |")
+    print("+" + "-"*38 + "+")
 
+def main():
+    while True:
+        print_menu()
+        user_choice = input("Enter your choice (1/2/3): ")
 
+        if user_choice == '1':
+            url = input("Enter the YouTube video URL: ")
+            output_path = 'C:/Songs/'  # Adjust the output path as needed
+            download_and_rename_audio(url, output_path)
+            input("Press Enter to continue...")
+
+        elif user_choice == '2':
+            file_path = input("Enter the file path containing YouTube video URLs: ")
+            output_path = 'C:/Songs/'  # Adjust the output path as needed
+
+            try:
+                with open(file_path, 'r') as file:
+                    urls = file.readlines()
+                    for url in urls:
+                        url = url.strip()  # Remove any leading/trailing whitespace or newline characters
+                        if url:  # Check if the URL is not empty
+                            download_and_rename_audio(url, output_path)
+                            input("Press Enter to continue...")
+                        else:
+                            print("File is empty!!")
+            except Exception as e:
+                print(f"Error reading URLs from file: {e}")
+                input("Press Enter to continue...")
+
+        elif user_choice == '3':
+            print("Exiting the program. Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
+            input("Press Enter to continue...")
+
+if __name__ == "__main__":
+    main()
